ecdfe516871a96dd8aa55150cca6b7cda06fb731
jvaquero24 <67736596+jvaquero24@users.noreply.github.com>
2021-06-20 22:24:19 +0200

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "resource_group_name" {
+  type        = string
+}
+variable "location" {
+  type        = string
+}
+variable "name_Service_Plan" {
+  type        = string
+}
+variable "name_web_app" {
+  type        = string
+}
